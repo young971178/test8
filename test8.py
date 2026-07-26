2fd0ddfacc9840abc0905aa9efcb6d0632658bf5
young971178 <young971178@gmail.com>
2026-07-26 13:09:40 +0900

    Update test8.py

--- test8.py
+++ test8.py
@@ -60,7 +60,7 @@ def go_module(module_name):
     st.rerun()
 
 if st.session_state.page == "main":
-    st.markdown(f"<h2>접속자: {st.session_state.user}</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h2>{st.session_state.user}님의 GDX 아카데이 입니다.</h2>", unsafe_allow_html=True)
     
     st.markdown("### 🏆 배지 획득 현황")
     st.markdown("<hr style='margin:0; padding-bottom:15px;'/>", unsafe_allow_html=True)
